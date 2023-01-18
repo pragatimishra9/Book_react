bcaaccc23840093e4f19f79fab8bd6332d5141d8
pragatimishra9 <mishrapragati557@gmail.com>
2023-01-18 16:24:31 +0530

    api port changed

--- BookAnAppointment_react/frontend/src/Actions/Request.js
+++ BookAnAppointment_react/frontend/src/Actions/Request.js
@@ -1,8 +1,8 @@
 const Request = {
-    auth: 'http://127.0.0.1:8000/api/v1/auth/accounts',
-    doctorInfo: 'http://127.0.0.1:8000/api/v1/doctor/info',
-    bookAppointment: 'http://127.0.0.1:8000/api/v1/doctor/bookAppointment',
-    updateAppointment: 'http://127.0.0.1:8000/api/v1/doctor/update_an_appointment',
-    filterAppointment: 'http://127.0.0.1:8000/api/v1/doctor/reports',
+    auth: 'http://18.183.15.206:8000//api/v1/auth/accounts',
+    doctorInfo: 'http://18.183.15.206:8000//api/v1/doctor/info',
+    bookAppointment: 'http://18.183.15.206:8000//api/v1/doctor/bookAppointment',
+    updateAppointment: 'http://18.183.15.206:8000//api/v1/doctor/update_an_appointment',
+    filterAppointment: 'http://18.183.15.206:8000//api/v1/doctor/reports',
 }
 export default Request;
